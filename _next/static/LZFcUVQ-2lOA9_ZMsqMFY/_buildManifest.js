9fdf79b8bda7c9905be532aea8def6cba608a130
quietsato <quietsato@users.noreply.github.com>
2022-07-13 07:42:04 +0000

    deploy: 0a77b64acc1194a8fd91da6b6bf71011e74bce83

--- _next/static/LZFcUVQ-2lOA9_ZMsqMFY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b3d3d1c759873163.css","static/chunks/pages/index-88926ac21345badd.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
